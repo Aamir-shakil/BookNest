f79fd85641334e24b236649a9382c2d837365fc6
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-25 12:41:48 +0000

    Fix: Function created in wrong file

--- app/controllers/Books.php
+++ app/controllers/Books.php
@@ -1,10 +1,4 @@
 <?php
 class Books extends Controller{
-    //Function to get all books
-    public function getAllBooks() {
-        $stmt = $this->db->prepare("SELECT * FROM books ORDER BY title ASC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 }
 ?>
--- app/models/Book.php
+++ app/models/Book.php
@@ -1,3 +1,11 @@
 <?php
-class Book extends Model{}
+class Book extends Model{
+      //Function to get all books
+      public function getAllBooks() {
+        $stmt = $this->db->prepare("SELECT * FROM books ORDER BY title ASC");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
 ?>
