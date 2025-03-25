7410352f85cb60c6bb921981796ac4b7f743e1a0
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-25 12:48:10 +0000

    Feat: Function in conbtroller  to handle request to display books

--- app/controllers/Books.php
+++ app/controllers/Books.php
@@ -1,4 +1,14 @@
 <?php
-class Books extends Controller{
+class Books extends Controller
+{
+
+    public function index()
+    {
+        $bookModel = $this->model('Book');
+        $books = $bookModel->getAllBooks();
+        $this->view('books/index', ['books' => $books]);
+    }
 }
+// This controller is responsible for handling the request to display all books. It calls the getAllBooks() method from the Book model to retrieve all books from the database and passes them to the view for rendering. The view file will then display the list of books to the user.
+
 ?>
