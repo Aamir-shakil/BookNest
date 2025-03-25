e23736eb420a56a890564d22917c7eea49b87827
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-25 12:50:10 +0000

    Feat:Added book index view to display book list

--- app/views/Books/index.php
+++ app/views/Books/index.php
@@ -6,6 +6,17 @@
     <title>BookNest Search Books</title>
 </head>
 <body>
+<h1>Browse Books</h1>
+    <div class="book-list">
+        <?php foreach ($data['books'] as $book): ?>
+            <div class="book-item">
+                <h2><?php echo htmlspecialchars($book['title']); ?></h2>
+                <p>Author: <?php echo htmlspecialchars($book['author']); ?></p>
+                <p>Price: $<?php echo htmlspecialchars($book['price']); ?></p>
+                <a href="book.php?id=<?php echo $book['id']; ?>">View Details</a>
+            </div>
+        <?php endforeach; ?>
+    </div>
     
 </body>
 </html>
