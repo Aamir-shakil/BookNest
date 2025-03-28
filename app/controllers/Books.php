78d94f474af3a8b48ba303583cc62e97b586e850
Aamir-shakil <aamirshakil21@gmail.com>
2025-03-28 11:49:46 +0000

    Feat:Function to retrieve book from databse

--- app/controllers/Books.php
+++ app/controllers/Books.php
@@ -4,11 +4,20 @@ class Books extends Controller
 
     public function index()
     {
-        $bookModel = $this->model('Book');
-        $books = $bookModel->getAllBooks();
-        $this->view('books/index', ['books' => $books]);
+        require_once '../app/models/Book.php';
+        $bookModel = new Book();
+
+        // Get search parameters
+        $query = isset($_GET['query']) ? trim($_GET['query']) : '';
+        $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
+
+        // Fetch books based on search query
+        $books = $bookModel->searchBooks($query, $filter);
+
+        require_once '../app/views/books/index.php';
     }
 }
+
 // This controller is responsible for handling the request to display all books. It calls the getAllBooks() method from the Book model to retrieve all books from the database and passes them to the view for rendering. The view file will then display the list of books to the user.
 
 ?>
--- app/models/Book.php
+++ app/models/Book.php
@@ -6,6 +6,26 @@ class Book extends Model{
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    public function searchBooks($query, $filter) {
+      $sql = "SELECT * FROM books";
+      $params = [];
+
+      if (!empty($query)) {
+          if ($filter == 'title') {
+              $sql .= " WHERE title LIKE :query";
+          } elseif ($filter == 'author') {
+              $sql .= " WHERE author LIKE :query";
+          } else {
+              $sql .= " WHERE title LIKE :query OR author LIKE :query";
+          }
+          $params['query'] = "%$query%";
+      }
+
+      $stmt = $this->db->prepare($sql);
+      $stmt->execute($params);
+      return $stmt->fetchAll();
+  }
 }
+
 
 ?>
