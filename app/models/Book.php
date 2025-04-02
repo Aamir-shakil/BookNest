c684ee75c68aa3e41c2fe07e87144bac77e291bd
Aamir-shakil <aamirshakil21@gmail.com>
2025-04-02 12:56:17 +0100

    Fix: query used incorectly should be '?'

--- app/models/Book.php
+++ app/models/Book.php
@@ -1,30 +1,35 @@
 <?php
 class Book extends Model{
-      //Function to get all books
-      public function getAllBooks() {
-        $stmt = $this->db->prepare("SELECT * FROM books ORDER BY title ASC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Function to get all books
+public function getAllBooks() {
+    $stmt = $this->db->prepare("SELECT * FROM books ORDER BY title ASC");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+public function searchBooks($query, $filter) {
+    $sql = "SELECT * FROM books WHERE 1=1"; // preventing where from disrupting the query
+    $params = [];
+
+    if (!empty($query)) {
+        if ($filter == 'title') {
+            $sql .= " AND title LIKE ?";
+            $params[] = "%$query%";
+        } elseif ($filter == 'author') {
+            $sql .= " AND author LIKE ?";
+            $params[] = "%$query%";
+        } else {
+            // "All" filter - search in both title and author
+            $sql .= " AND (title LIKE ? OR author LIKE ?)";
+            $params[] = "%$query%";
+            $params[] = "%$query%";
+        }
     }
-    public function searchBooks($query, $filter) {
-      $sql = "SELECT * FROM books WHERE 1=1"; // preventing Where conflicts
-      $params = [];
 
-      if (!empty($query)) {
-          if ($filter == 'title') {
-              $sql .= " WHERE title LIKE :query";
-          } elseif ($filter == 'author') {
-              $sql .= " WHERE author LIKE :query";
-          } else {
-              $sql .= " WHERE title LIKE :query OR author LIKE :query";
-          }
-          $params['query'] = "%$query%";
-      }
-
-      $stmt = $this->db->prepare($sql);
-      $stmt->execute($params);
-      return $stmt->fetchAll();
-  }
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute($params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 }
 
 
